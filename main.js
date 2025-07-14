53011e6e65fb1cedcbaecd0685efe3027e1d9439
Fra9125 <francescoserratoref19@gmail.com>
2025-07-14 18:59:35 +0200

    Game function and basic html document

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ function computerChoice(){
     let index = Math.floor(Math.random()*3)
     return choices[index];
 }
-let sceltaDelComputer = computerChoice()
-
-
+function humanChoice() {
+    const userChoices = ["Rock", "Paper", "Scissors"];
+    let choice = prompt("What do you choose?")
+    return choice;
+}
+let sceltaDelComputer = computerChoice().toLowerCase()
+let sceltaDellUmano = humanChoice().toLowerCase()
+alert(sceltaDelComputer)
+alert(sceltaDellUmano)
+function partita (sceltaDelComputer, sceltaDellUmano){
+    if (sceltaDelComputer === sceltaDellUmano){
+        console.log("DRAW!")
+    }
+    else if (sceltaDelComputer === "rock" && sceltaDellUmano === "scissors"||
+             sceltaDelComputer === "paper" && sceltaDellUmano === "rock" ||
+             sceltaDelComputer === "scissors" && sceltaDellUmano === "paper"){
+                console.log("Computer win!")
+             }
+    else {
+        console.log("Human wins!")
+    }
+  
+}
+partita(sceltaDelComputer,sceltaDellUmano)
