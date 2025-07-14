9f8cf046d9dcb0a6a482140be6b9fc57b5e83fd7
Fra9125 <francescoserratoref19@gmail.com>
2025-07-14 19:45:38 +0200

    some fixes
    Capital letters from the choices removed, fixed the while loop ecc

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
+//Funzione che crea la scelta del computer
 function computerChoice(){
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random()*3)
     return choices[index];
 }
+//Funzione per la scelta del giocatore
 function humanChoice() {
-    const userChoices = ["Rock", "Paper", "Scissors"];
+    const userChoices = ["rock", "paper", "scissors"];
     let choice = prompt("What do you choose?")
     return choice;
 }
-let sceltaDelComputer = computerChoice().toLowerCase()
-let sceltaDellUmano = humanChoice().toLowerCase()
-function partita (sceltaDelComputer, sceltaDellUmano){
-    if (sceltaDelComputer === sceltaDellUmano){
+//Funzione per lo svolgimento del singolo round
+function round (computerCho, humanCho){
+    if (computerCho === humanCho){
         console.log("DRAW!")
     }
-    else if (sceltaDelComputer === "rock" && sceltaDellUmano === "scissors"||
-             sceltaDelComputer === "paper" && sceltaDellUmano === "rock" ||
-             sceltaDelComputer === "scissors" && sceltaDellUmano === "paper"){
+    else if (computerCho === "rock" && humanCho === "scissors"||
+             computerCho === "paper" && humanCho === "rock" ||
+             computerCho === "scissors" && humanCho === "paper"){
                 console.log("Computer wins!")
+                computerScore++
                 }
     else {
         console.log("Human wins!")
+        humanScore++
     }
   
 }
-partita(sceltaDelComputer,sceltaDellUmano)
+//Dichiarazione variabili globali
+let humanScore = 0
+let computerScore = 0
+//Funzione partita
+function partita(){
+    while (true){
+        let question = prompt("You wanna play?")
+        if (question.toLowerCase() === "yes"){
+            let sceltaDelComputer = computerChoice().toLowerCase()
+            let sceltaDellUmano = humanChoice().toLowerCase()
+            //stampa delle scelte
+            console.log("Your choice is: " + sceltaDellUmano)
+            console.log("Meanwhile the computer chose: " + sceltaDelComputer)
+            round(sceltaDelComputer, sceltaDellUmano)
+                        //stampa dei punteggi
+            console.log("Computer points: " +computerScore)
+            console.log("Human points: " + humanScore)     
+        }
+        else {
+            console.log("Game is over!")
+            console.log(`The final results are: ${humanScore} for you and ${computerScore} for the computer!`)
+            break
+        }
+    }
+
+}
+partita()
